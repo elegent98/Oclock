8c1c6a4ea9cf69f95cdd337fa766771c606de891
Bobur <bobur@email.com>
2024-03-24 15:25:49 +0500

    Oclock

---
+++ script.js
@@ -0,0 +1,46 @@
+const sr = document.querySelector("#sr")
+const mr = document.querySelector("#mr")
+const hr = document.querySelector("#hr")
+const hours = document.getElementById("hours")
+const minutes = document.getElementById("minutes")
+const seconds = document.getElementById("seconds")
+const ampm = document.getElementById("ampm")
+
+
+
+
+
+setInterval(() => {
+    let day = new Date()
+    let hh = day.getHours()*30
+    let mm = day.getMinutes()*6
+    let ss = day.getSeconds() * 6
+
+    hr.style.transform = `rotateZ(${hh + (mm / 12)}deg)`
+    mr.style.transform = `rotate(${mm}deg)`
+    sr.style.transform = `rotate(${ss}deg)`
+})
+
+
+
+let h = new Date().getHours()
+let m = new Date().getMinutes()
+let s = new Date().getSeconds()
+
+h = (h < 10) ? "0" + h : h
+m = (m < 10) ? "0" + m : m
+s = (s < 10)? "0" + s : s
+
+let am = (h >= 12) ? "PM" : "AM"
+
+if (h > 12) {
+    h = h - 12
+}
+hours.innerHTML = h
+minutes.innerHTML = m
+seconds.innerHTML = s
+ampm.innerHTML = am
+
+
+
+
